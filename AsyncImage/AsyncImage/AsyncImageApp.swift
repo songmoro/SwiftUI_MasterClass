39603bcde5d019467b5b1782c6989abb54af058b
songmoro <97587585+songmoro@users.noreply.github.com>
2023-07-11 02:57:04 +0900

    Feat: AsyncImage

---
+++ AsyncImage/AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by 송재훈 on 2023/07/06.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
